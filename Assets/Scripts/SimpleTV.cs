4acca81ebf207b3ed3c6e27ddde8f83368b297aa
fadedawayyy <gergot.gg@gmail.com>
2026-02-03 21:18:15 +0200

    Izveidots televizors ar 2 kanaliem

---
+++ Assets/Scripts/SimpleTV.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SimpleTV : MonoBehaviour
+{
+    public GameObject tvScreen;
+    public GameObject[] channels;
+
+    
+    public void PowerButton(bool isOn)
+    {
+        tvScreen.SetActive(isOn);
+    }
+
+   
+    public void ShowChannel(int channelIndex)
+    {
+        
+        for (int i = 0; i < channels.Length; i++)
+        {
+            channels[i].SetActive(false);
+        }
+
+        
+        if (channelIndex >= 0 && channelIndex < channels.Length)
+        {
+            channels[channelIndex].SetActive(true);
+        }
+    } 
+
+    
+    public void ChangeVolume(float amount)
+    {
+        
+        AudioListener.volume = AudioListener.volume + amount;
+
+        
+        if (AudioListener.volume < 0) AudioListener.volume = 0;
+        if (AudioListener.volume > 1) AudioListener.volume = 1;
+    }
+}
