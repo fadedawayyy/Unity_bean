652fbd67a7a36bba9912619f749b32fb3f3b3f93
fadedawayyy <gergot.gg@gmail.com>
2026-02-22 20:14:46 +0200

    Pieveinots Galvena scena un iziešanas taustiņas

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void LoadScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+
+    public void QuitGame()
+    {
+        
+        Application.Quit();
+
+        
+        Debug.Log("Game closed!");
+    }
+}
+
